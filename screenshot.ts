ee3a3627f144c94d4ce14731676268b34dd9f021
stressfuluser <stressfuluser@proton.me>
2025-11-16 23:57:10 +0800

    Make screenshot for different screen size

---
+++ screenshot.ts
@@ -0,0 +1,28 @@
+import { devices, chromium } from "playwright";
+
+type DeviceName = keyof {
+  [K in keyof typeof devices as string extends K ? never : K]: unknown;
+};
+
+const names: DeviceName[] = [
+  "iPhone 6",
+  "iPhone 8 Plus",
+  "iPad Mini",
+  "iPad Pro 11",
+  "Desktop Chrome",
+  "Desktop Chrome HiDPI",
+];
+
+const browser = await chromium.connect("ws://0.0.0.0:4000");
+
+const screenshot = async (name: DeviceName) => {
+  const context = await browser.newContext(devices[name]);
+  const page = await context.newPage();
+  await page.goto("http://0.0.0.0:3000");
+  await page.screenshot({
+    path: `docs/${String(name).replaceAll(" ", "_")}.jpg`,
+  });
+};
+
+await Promise.all(names.map(screenshot));
+await browser.close();
